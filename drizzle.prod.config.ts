7fdbd5913e57dd37fe7e78bdcb4142e88730119a
hx <1570972259@qq.com>
2025-04-28 07:42:05 +0800

    feat: url

--- drizzle.prod.config.ts
+++ drizzle.prod.config.ts
@@ -5,7 +5,7 @@ export default {
   out: "./drizzle/migrations",
   dialect: "sqlite", // 关键变更：不再需要 driver 字段
   dbCredentials: {
-    url: "sqlite:/tmp/db/prod.db", // 使用绝对路径
+    url: "sqlite:///tmp/db/prod.db", // 使用绝对路径
   },
   verbose: true, // 可选：显示详细日志
   strict: true, // 可选：严格模式
