fc13915f8394c64a89d6e3f1452c75f10f7e485b
hx <1570972259@qq.com>
2025-04-27 21:42:55 +0800

    feat: git cached

--- src/db/schema.ts
+++
@@ -1,9 +0,0 @@
-// 表结构定义
-// lib/schema.ts
-import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
-
-export const posts = sqliteTable("posts", {
-  id: integer("id").primaryKey({ autoIncrement: true }),
-  title: text("title").notNull(),
-  content: text("content"),
-});
