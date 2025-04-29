0f2c07475459e2fd2827cad22ce33311c4d682e0
hx <1570972259@qq.com>
2025-04-29 21:58:24 +0800

    feat: rss_source

---
+++ src/app/rss_source/new/page.tsx
@@ -0,0 +1,95 @@
+"use client";
+import { useRouter } from "next/navigation";
+
+export default function RssPage() {
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget as HTMLFormElement);
+
+    const name = formData.get("name"); // 获取 input[name="name"] 的值
+    const description = formData.get("description");
+    const url = formData.get("url");
+    const res = await fetch("/api/rss_sources/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, url, description }),
+    });
+
+    const result = await res.json();
+    if (result.code == 200) {
+      router.push("/rss_source"); // 跳转到某个页面
+    }
+
+    console.log(result);
+  };
+  return (
+    <div>
+      <form
+        onSubmit={handleSubmit}
+        className="max-w-md mx-auto space-y-4 p-6 bg-white rounded-lg shadow-md"
+      >
+        <div>
+          <label
+            htmlFor="name"
+            className="block text-sm font-medium text-gray-700 mb-1"
+          >
+            名称
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            required
+            minLength={3}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+
+        <div>
+          <label
+            htmlFor="url"
+            className="block text-sm font-medium text-gray-700 mb-1"
+          >
+            RSS 链接
+          </label>
+          <input
+            type="text"
+            id="url"
+            name="url"
+            required
+            minLength={3}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+
+        <div>
+          <label
+            htmlFor="description"
+            className="block text-sm font-medium text-gray-700 mb-1"
+          >
+            描述
+          </label>
+          <textarea
+            id="description"
+            name="description"
+            required
+            minLength={10}
+            rows={6}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className={`w-full py-2 px-4 rounded-md text-white font-medium ${"bg-blue-600 hover:bg-blue-700"}`}
+        >
+          添加 RSS
+        </button>
+      </form>
+    </div>
+  );
+}
---
+++ src/app/rss_source/page.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+type RssSourcesType = {
+  name: string;
+  url: string;
+  description: string;
+};
+
+export default function RssSourcesPage() {
+  const [data, setData] = useState<RssSourcesType[]>([]);
+  const [total, setTotal] = useState<number>(0);
+  const [loading, setLoading] = useState(true);
+
+  const [page, setPage] = useState(1);
+  const pageSize = 10;
+
+  const totalPages = Math.ceil(total / pageSize);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const res = await fetch("/api/rss_sources/page", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ page, pageSize }),
+      });
+
+      const result = await res.json();
+      console.log("result=======", result);
+      setData(result.data?.data); // 确保 API 返回 { data: [...] }
+      setTotal(result.data?.total);
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [page]);
+
+  const handlePrev = () => setPage((prev) => Math.max(prev - 1, 1));
+  const handleNext = () => setPage((prev) => Math.min(prev + 1, totalPages));
+
+  return (
+    <div className="max-w-2xl mx-auto p-4">
+      <section className="flex mb-4 items-center justify-between">
+        <h1 className="text-xl font-bold ">RSS Sources</h1>
+        <a className="text-blue-500 cursor-pointer" href="/rss_source/new">
+          +新增
+        </a>
+      </section>
+
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <ul className="space-y-2">
+            {data.map((item, index) => (
+              <li key={index} className="p-3 border rounded">
+                {item.name} – {item.url}
+              </li>
+            ))}
+          </ul>
+
+          <div className="flex justify-between mt-6 items-center">
+            <button
+              onClick={handlePrev}
+              disabled={page === 1}
+              className="px-3 py-1 border rounded disabled:opacity-50"
+            >
+              Previous
+            </button>
+            <span>
+              Page {page} of {totalPages}
+            </span>
+            <button
+              onClick={handleNext}
+              disabled={page === totalPages}
+              className="px-3 py-1 border rounded disabled:opacity-50"
+            >
+              Next
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
