b78555dd81c499c18ec65ec6945874f34938d66f
hx <1570972259@qq.com>
2025-05-30 20:46:01 +0800

    feat: nav icon

--- src/app/rss_source/page.tsx
+++ src/app/rss_source/page.tsx
@@ -71,7 +71,7 @@ export default function RssSourcesPage() {
   return (
     <div className="max-w-2xl mx-auto p-4">
       <section className="flex mb-4 items-center justify-between border-b border-gray-400">
-        <h1 className="text-xl font-bold ">RSS Sources</h1>
+        <h1 className="text-xl font-bold ">rss sources</h1>
         <a className="text-blue-500 cursor-pointer" href="/rss_source/new">
           +新增
         </a>
--- src/app/shortnews/layout.tsx
+++ src/app/shortnews/layout.tsx
@@ -1,11 +1,18 @@
+import Footer from "@/components/Footer";
+import Navigation from "@/components/Header/Navigation";
+
 export default function PostLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <main className="min-h-screen max-w-3xl m-auto py-6 px-3 container-paper">
-      {children}
-    </main>
+    <>
+      <Navigation />
+      <main className="min-h-screen max-w-3xl m-auto pt-20 px-3 container-paper">
+        {children}
+      </main>
+      <Footer />
+    </>
   );
 }
